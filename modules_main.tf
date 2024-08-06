7673d364331e152ec42936eaa7d9b2183274ef3b
manbuchi <manikanta.reddy.buchi@oracle.com>
2024-08-06 19:18:03 +0530

    update

--- modules_main.tf
+++ modules_main.tf
@@ -1,18 +1,18 @@
-# module "vcn" {
-#   source = "./modules/vcn"
-#   compartment_id = var.compartment_id  
-#   create_new_vcn = var.create_new_vcn
-#   vcn_cidr       = var.vcn_cidr
-#   vcn_name       = var.vcn_name
-#   vcn_id         = var.vcn_id
-# }
+module "vcn" {
+  source = "./modules/vcn"
+  compartment_id = var.compartment_id  
+  create_new_vcn = var.create_new_vcn
+  vcn_cidr       = var.vcn_cidr
+  vcn_name       = var.vcn_name
+  vcn_id         = var.vcn_id
+}
 
 resource "oci_apigateway_gateway" "api_gateway" {
   compartment_id = var.compartment_id
   display_name   = var.api_gateway_name
   endpoint_type  = "PUBLIC"
-  # subnet_id      = var.create_new_vcn ? module.vcn.public_subnet_id : var.public_subnet_id
-    subnet_id      = var.public_subnet_id
+  subnet_id      = var.create_new_vcn ? module.vcn.public_subnet_id : var.public_subnet_id
+    # subnet_id      = var.public_subnet_id
 
   depends_on = [module.vcn]
 }
---
+++ terraform.tfvars
@@ -0,0 +1,7 @@
+compartment_id = "ocid1.compartment.oc1..aaaaaaaal2o4vaqyt3kiwvezviozntgck5pp33eihmgdvofrxpl4fiiyztaq"
+create_new_vcn = true
+vcn_id = ""  # Leave empty if create_new_vcn == true
+vcn_cidr = "10.0.0.0/16"
+vcn_name = "example-vcn"
+api_gateway_name = "example-api-gateway"
+public_subnet_id="ocid1.subnet.oc1.iad.aaaaaaaaek43v3qrcr2aycpdbav5ufe3zq32trulojazand42qdzi4iejd7q"
